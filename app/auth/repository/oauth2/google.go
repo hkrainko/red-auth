bc97b0e747e16bf497b38e009ac358bb2968133f
hkrainko <>
2020-11-03 02:32:40 +0800

    no message

--- app/auth/repository/oauth2/google.go
+++ app/auth/repository/oauth2/google.go
@@ -3,7 +3,6 @@ package oauth2
 import (
 	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/google"
-	"os"
 )
 
 var (
@@ -13,8 +12,8 @@ var (
 func init() {
 	googleOauthConfig = &oauth2.Config{
 		RedirectURL:  "http://localhost:8080/callback",
-		ClientID:     os.Getenv("GOOGLE_CLIENT_ID"),
-		ClientSecret: os.Getenv("GOOGLE_CLIENT_SECRET"),
+		ClientID:     "601833756814-1n1uo2jp77sp888mjsrsl1fmru69kvhb.apps.googleusercontent.com",
+		ClientSecret: "yyZUseQoTSYS8tT0MP-M9MrL",
 		Scopes:       []string{"https://www.googleapis.com/auth/userinfo.email"},
 		Endpoint:     google.Endpoint,
 	}
